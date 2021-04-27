ec73e5601a6f9aa548348849775684ad70e56a7e
Tamir Tevet <tevet.tamir@gmail.com>
2021-04-27 08:32:31 +0300

    add logic

--- analyzer/src/main/java/bank/poalim/analyzer/controller/AnalyzerController.java
+++ analyzer/src/main/java/bank/poalim/analyzer/controller/AnalyzerController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Slf4j
 @RestController

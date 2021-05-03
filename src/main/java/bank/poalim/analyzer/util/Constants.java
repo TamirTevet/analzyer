b87e148b1637b3e64dbff5ebabb3af613d1930ce
Tamir Tevet <tevet.tamir@gmail.com>
2021-05-03 10:02:41 +0300

    fix project

---
+++ src/main/java/bank/poalim/analyzer/util/Constants.java
@@ -0,0 +1,4 @@
+package bank.poalim.analyzer.util;
+
+public class Constants {
+}
---
+++ src/test/java/bank/poalim/analyzer/AnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package bank.poalim.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class AnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
